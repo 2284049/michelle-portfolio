b97160d8775083513d9ab8db229ddb7ffa6ae738
2284049 <70497897+2284049@users.noreply.github.com>
2020-10-13 07:01:34 -0700

    get bootstrap into react app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./style/master.scss";
 
 function App() {
    return (
       <>
-         <h1>Hello World</h1>
+         <h1 className="text-primary">Hello World</h1>
       </>
    );
 }
